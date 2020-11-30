55806c5fa196d3c0a9628603845607612d9f42b1
Bruno Menendes Rodrigues <menendesbruno@gmail.com>
2020-11-30 20:01:47 -0300

    Primeiros Pasos

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerodevezes = 5;
+            for (int i = 0; i < numerodevezes; i++ ){
+                Console.WriteLine($"Bem vindo ao curso de .NET{i}");
+            }
+
+        }
+    }
+}
